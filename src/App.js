c901c03367f0fe2b8ea105b646d2d8c19d167a80
melkam <melkammekonnen899@gmail.com>
2020-12-24 12:44:01 +0300

    Setting app my app with a table view, installed reactstrap

---
+++ src/App.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react';
+import { Table, Button } from 'reactstrap';
+
+function App() {
+  return (
+    <div className="App container">
+      <Table>
+        <thead>
+          <tr>
+            <th>#</th>
+            <th>Title</th>
+            <th>Rating</th>
+            <th>Actions</th>
+          </tr>
+        </thead>
+
+        <tbody>
+          <tr>
+            <td>1</td>
+            <td>Book title</td>
+            <td>2.5</td>
+            <td>
+              <Button color="success" size="sm">Edit</Button>
+              <Button color="danger" size="sm">Delete</Button>
+            </td>
+          </tr>
+        </tbody>
+      </Table>
+    </div>
+  );
+}
+
+export default App;
